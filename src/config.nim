a54e2d91a259d0677531cc5b2b2f38151838f445
Paulo Carbalone <psoreto@outlook.com>
2022-06-11 15:45:46 -0300

    conf auto

--- src/config.nim
+++ src/config.nim
@@ -9,7 +9,4 @@ type
 proc loadConf*(): Conf =
   let text = readFile("./settings.json")
   let node = parseJson(text)
-
-  result = Conf(lock: node["lock"].getBool(),
-                port: node["port"].getInt(),
-                max_clients: node["max_clients"].getInt())
+  result = node.to(Conf)
